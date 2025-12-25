7e4ea097620d7f9bc70db34b8539d752b3325af9
schmeedy4 <4629318+schmeedy4@users.noreply.github.com>
2025-12-25 09:15:21 +0100

    Clean poject

--- config/config.php
+++ config/config.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 return [
     'db' => [
-        'dsn' => 'mysql:host=localhost;dbname=portfolio_tracker;charset=utf8mb4',
-        'user' => 'portfolio_user',
-        'password' => 'change_me',
+        'dsn' => 'mysql:host=localhost;dbname=stocks;charset=utf8mb4',
+        'user' => 'root',
+        'password' => '',
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
